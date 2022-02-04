6cf210844b5f8c5a8437aa761216d54814df8014
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 13:56:55 -0800

    add test for calculating Mercury life expectancy

--- __tests__/person.tests.js
+++ __tests__/person.tests.js
@@ -5,7 +5,7 @@ describe("Person", () => {
   let aPerson;
   beforeEach(() => {
     aPerson = new Person("Bob", 55, "desert", 2200, "coffee");
-    aPerson.lifeExpectancyEarth = calculateLifeExpectancy(aPerson.climate, aPerson.dailyCalories, aPerson.coffeeOrTea);
+    aPerson.earthLifeExpectancy = calculateLifeExpectancy(aPerson.climate, aPerson.dailyCalories, aPerson.coffeeOrTea);
   });
   
   test("Should create a person with a name and an earth age", () => {
@@ -14,26 +14,31 @@ describe("Person", () => {
     expect(aPerson.climate).toEqual("desert");
     expect(aPerson.dailyCalories).toEqual(2200);
     expect(aPerson.coffeeOrTea).toEqual("coffee");
-    expect(aPerson.lifeExpectancyEarth).toEqual(78);
+    expect(aPerson.earthLifeExpectancy).toEqual(78);
   });
 
   test("Should calculate the age in Mercury years", () => {
-    aPerson.mercuryYears = aPerson.planetCalc(.24);
+    aPerson.mercuryYears = aPerson.planetCalc(aPerson.earthAge, .24);
     expect(aPerson.mercuryYears).toEqual(229);
   });
 
+  test("Should calculate the expected life age in Mercury years", () => {
+    aPerson.mercuryLifeExpectancy = aPerson.planetCalc(aPerson.earthLifeExpectancy, .24);
+    expect(aPerson.mercuryLifeExpectancy).toEqual(325);
+  });
+
   test("Should calculate the age in Venus years", () => {
-    aPerson.venusYears = aPerson.planetCalc(.62);
+    aPerson.venusYears = aPerson.planetCalc(aPerson.earthAge, .62);
     expect(aPerson.venusYears).toEqual(89);
   });
 
   test("Should calculate the age in Mars years", () => {
-    aPerson.marsYears = aPerson.planetCalc(1.88);
+    aPerson.marsYears = aPerson.planetCalc(aPerson.earthAge, 1.88);
     expect(aPerson.marsYears).toEqual(29);
   });
 
   test("Should calculate the age in Jupiter years", () => {
-    aPerson.jupiterYears = aPerson.planetCalc(11.86);
+    aPerson.jupiterYears = aPerson.planetCalc(aPerson.earthAge, 11.86);
     expect(aPerson.jupiterYears).toEqual(5);
   });
 
