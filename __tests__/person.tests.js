83e17d9c366865a824a6fe1b85e3fee886a9e550
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 14:12:02 -0800

    add a test for getting years left in Mercury Years

--- __tests__/person.tests.js
+++ __tests__/person.tests.js
@@ -29,12 +29,12 @@ describe("Person", () => {
 
   test("Should calculate the age in Venus years", () => {
     aPerson.venusYears = aPerson.planetCalc(aPerson.earthAge, .62);
-    expect(aPerson.venusYears).toEqual(89);
+    expect(aPerson.venusYears).toEqual(88);
   });
 
   test("Should calculate the expected life age in Venus years", () => {
     aPerson.venusLifeExpectancy = aPerson.planetCalc(aPerson.earthLifeExpectancy, .62);
-    expect(aPerson.venusLifeExpectancy).toEqual(126);
+    expect(aPerson.venusLifeExpectancy).toEqual(125);
   });
 
   test("Should calculate the age in Mars years", () => {
@@ -42,9 +42,25 @@ describe("Person", () => {
     expect(aPerson.marsYears).toEqual(29);
   });
 
+  test("Should calculate the expected life age in Mars years", () => {
+    aPerson.marsLifeExpectancy = aPerson.planetCalc(aPerson.earthLifeExpectancy, 1.88);
+    expect(aPerson.marsLifeExpectancy).toEqual(41);
+  });
+
   test("Should calculate the age in Jupiter years", () => {
     aPerson.jupiterYears = aPerson.planetCalc(aPerson.earthAge, 11.86);
-    expect(aPerson.jupiterYears).toEqual(5);
+    expect(aPerson.jupiterYears).toEqual(4);
+  });
+
+  test("Should calculate the expected life age in Jupiter years", () => {
+    aPerson.jupiterLifeExpectancy = aPerson.planetCalc(aPerson.earthLifeExpectancy, 11.86);
+    expect(aPerson.jupiterLifeExpectancy).toEqual(6);
+  });
+
+  test("Should return how many years they have left in Mercury Years", () => {
+    aPerson.mercuryYears = aPerson.planetCalc(aPerson.earthAge, .24);
+    aPerson.mercuryLifeExpectancy = aPerson.planetCalc(aPerson.earthLifeExpectancy, .24);
+    expect(aPerson.getYearsLeft("Mercury")).toEqual(106);
   });
 
 });
