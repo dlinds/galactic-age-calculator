ef089dcd7773b2fec102dfea300684482eadc19b
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 11:36:50 -0800

    correct the Mercury age test again

--- __tests__/person.tests.js
+++ __tests__/person.tests.js
@@ -16,7 +16,7 @@ describe("Person", () => {
 
   test("Should calculate the age in Mercury years", () => {
     aPerson.calcMercury();
-    expect(aPerson.mercuryYears).toEqual(342);
+    expect(aPerson.mercuryYears).toEqual(229);
   });
 
   // test("Should calculate the age in Venus years", () => {
