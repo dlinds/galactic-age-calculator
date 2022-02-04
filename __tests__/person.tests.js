cce40cd39535b288947495178de9e47e19dbf719
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 08:51:13 -0800

    create age in Mercury years test

--- __tests__/person.tests.js
+++ __tests__/person.tests.js
@@ -1,4 +1,4 @@
-import Person from "../src/js/person.js";
+import { Person } from "../src/js/person.js";
 
 describe("Person", () => {
   let aPerson;
@@ -9,6 +9,11 @@ describe("Person", () => {
   test("Should create a person with a name and an earth age", () => {
     expect(aPerson.name).toEqual("Bob");
     expect(aPerson.earthAge).toEqual(55);
+  });
+
+  test("Should return age in Mercury years", () => {
+    aPerson.calcMercury();
+    expect(aPerson.mercuryYears).toEqual(13.2);
   });
 
 });
