7b079bae176097434f7a6c9f1975f155f02d1342
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 12:30:05 -0800

    write out logic for life expectancy function, pass tests

--- src/js/lifeExpectancy.js
+++ src/js/lifeExpectancy.js
@@ -1,3 +1,25 @@
 export function calculateLifeExpectancy (climate, dailyCalories, coffeeOrTea) {
-  return 78;
+  let expectedAge = 0;
+  switch (climate) {
+    case "desert":
+      expectedAge += 45;
+      break;
+    case "tropical":
+      expectedAge += 50;
+      break;
+    case "plains":
+      expectedAge += 55;
+      break;
+    default:
+      return false;
+  }
+  
+  expectedAge += (20 - (dailyCalories /= 100)) + 20;
+
+  if (coffeeOrTea === "coffee") {
+    expectedAge += 15;
+  } else {
+    expectedAge += 20;
+  }
+  return expectedAge;
 }
