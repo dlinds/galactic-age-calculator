3f87c08969ee76922d54a9b0b8d5b3b6bbd83d2a
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 12:23:45 -0800

    add test for plains branch

--- __tests__/lifeExpectancy.tests.js
+++ __tests__/lifeExpectancy.tests.js
@@ -5,5 +5,6 @@ describe("calculateLifeExpectancy", () => {
   test("Should return an estimated life expectancy", () => {
     expect(calculateLifeExpectancy("desert", 2200, "coffee")).toEqual(78);
     expect(calculateLifeExpectancy("tropical", 1800, "tea")).toEqual(92);
+    expect(calculateLifeExpectancy("plains", 2000, "tea")).toEqual(95);
   });
 });
