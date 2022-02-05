7ac4de1b1695872e79033ba88fc5556f8d026907
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-04 16:11:42 -0800

    add basic interface to collect information

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import *** from './js/*** */.js';
+// import { Person } from "../src/js/person.js";
+// import { calculateLifeExpectancy } from "../src/js/lifeExpectancy.js";
+
+$(document).ready(function () {
+  
+});
